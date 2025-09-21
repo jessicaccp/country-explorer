980abd696b93e4793d25c41f657fe0a6f9059d2e
Jessica Patricio <jessicacacau@gmail.com>
2025-09-20 22:55:47 -0300

    feat: add theme toggle and environment variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
+import ThemeToggle from './ThemeToggle'
 
 const Header = () => {
   return (
     <>
-      <header>
+      <header className='flex justify-between items-center'>
         <Link to='/'>
           <h1 className='text-left text-4xl sm:text-5xl  font-extrabold tracking-tight text-balance'>
             Country Explorer
           </h1>
         </Link>
+        <ThemeToggle />
       </header>
     </>
   )
---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { useTheme } from '@/hooks/useTheme'
+import { Moon, Sun } from 'lucide-react'
+
+const ThemeToggle = () => {
+  const { theme, toggleTheme } = useTheme()
+
+  return (
+    <Button
+      variant='ghost'
+      size='icon'
+      onClick={toggleTheme}
+      aria-label='Toggle theme'
+    >
+      {theme === 'light' ? (
+        <Moon className='h-5 w-5' />
+      ) : (
+        <Sun className='h-5 w-5' />
+      )}
+    </Button>
+  )
+}
+
+export default ThemeToggle
